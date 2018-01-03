f080afed01b3ce7389c7f7b3f04dec838787c1da
Almeida <rodolfo.almeida@accenture.com>
2018-01-03 16:00:25 -0300

    app configured

---
+++ app.js
@@ -0,0 +1,47 @@
+var path = require("path");
+var express = require("express");
+var zipdb = require("zippity-do-dah");
+var ForecastIo = require("forecastio");
+
+var app = express();
+// Creates an ForecastIO object with the API KEY
+var weather = new ForecastIo("e5f53bf74609702e85529facbaaac046");
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+    res.render("index");
+});
+
+//Captures the specified ZIP Code and passes it as req.params[0]
+app.get(/^\/(\d{5})$/, function (req, res, next) {
+    var zipcode = req.params[0];
+    var location = zipdb.zipcode(zipcode);
+    if (!location.zipcode) {
+        next();
+        return;
+    }
+
+    var latitude = location.latitude;
+    var longitude = location.longitude;
+
+    weather.forecast(latitude, longitude, function (err, data) {
+        if (err) {
+            next();
+            return;
+        }
+        res.json({
+            zipcode: zipcode,
+            temperature: data.currency.temperature
+        });
+    });
+});
+
+app.use(function (req, res) {
+    res.status(404).render("404");
+});
+
+app.listen(3000);
